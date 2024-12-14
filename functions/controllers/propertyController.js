a4d48341edc028a155e331a73534a848683feea1
Rajkumar Verma <rajkumarshaw624@gmail.com>
2024-12-14 20:25:18 +0530

    Remove space

--- functions/controllers/propertyController.js
+++ functions/controllers/propertyController.js
@@ -6,7 +6,7 @@ exports.getAllProperties = async (req, res) => {
     const properties = await Property.find();
     res.json(properties);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({message: err.message});
   }
 };
 
@@ -14,10 +14,10 @@ exports.getAllProperties = async (req, res) => {
 exports.getPropertyById = async (req, res) => {
   try {
     const property = await Property.findById(req.params.id);
-    if (!property) return res.status(404).json({ message: "Property not found" });
+    if (!property) return res.status(404).json({message: "Property not found"});
     res.json(property);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({message: err.message});
   }
 };
 
@@ -28,18 +28,18 @@ exports.createProperty = async (req, res) => {
     const savedProperty = await newProperty.save();
     res.status(201).json(savedProperty);
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(400).json({message: err.message});
   }
 };
 
 // Update a property by ID
 exports.updateProperty = async (req, res) => {
   try {
-    const updatedProperty = await Property.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!updatedProperty) return res.status(404).json({ message: "Property not found" });
+    const updatedProperty = await Property.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    if (!updatedProperty) return res.status(404).json({message: "Property not found"});
     res.json(updatedProperty);
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(400).json({message: err.message});
   }
 };
 
@@ -47,9 +47,9 @@ exports.updateProperty = async (req, res) => {
 exports.deleteProperty = async (req, res) => {
   try {
     const deletedProperty = await Property.findByIdAndDelete(req.params.id);
-    if (!deletedProperty) return res.status(404).json({ message: "Property not found" });
-    res.json({ message: "Property deleted successfully" });
+    if (!deletedProperty) return res.status(404).json({message: "Property not found"});
+    res.json({message: "Property deleted successfully"});
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({message: err.message});
   }
 };
