6a352489a0ac84b0329cb12cf0f1f3c3ac09c3ca
Rajkumar Verma <rajkumarshaw624@gmail.com>
2024-12-14 20:41:02 +0530

    Code Update

--- functions/controllers/connectController.js
+++ functions/controllers/connectController.js
@@ -2,40 +2,40 @@ const Connect = require("../models/Connect");
 
 // Get all connections
 exports.getAllConnections = async (req, res) =>{
-  try{
+  try {
     const connections = await Connect.find();
     res.json(connections);
-  }catch (err){
+  } catch (err){
     res.status(500).json({message: err.message});
   }
 };
 
 // Get connection by ID
 exports.getConnectionById = async (req, res) =>{
-  try{
+  try {
     const connection = await Connect.findById(req.params.id);
     if (!connection)
       return res.status(404).json({message: "Connection not found"});
     res.json(connection);
-  }catch (err){
+  } catch (err){
     res.status(500).json({message: err.message});
   }
 };
 
 // Create a new connection
 exports.createConnection = async (req, res) =>{
-  try{
+  try {
     const newConnection = new Connect(req.body);
     const savedConnection = await newConnection.save();
     res.status(201).json(savedConnection);
-  }catch (err){
+  } catch (err){
     res.status(400).json({message: err.message});
   }
 };
 
 // Update a connection by ID
 exports.updateConnection = async (req, res) =>{
-  try{
+  try {
     const updatedConnection = await Connect.findByIdAndUpdate(
       req.params.id,
       req.body,
@@ -44,19 +44,19 @@ exports.updateConnection = async (req, res) =>{
     if (!updatedConnection)
       return res.status(404).json({message: "Connection not found"});
     res.json(updatedConnection);
-  }catch (err){
+  } catch (err){
     res.status(400).json({message: err.message});
   }
 };
 
 // Delete a connection by ID
 exports.deleteConnection = async (req, res) =>{
-  try{
+  try {
     const deletedConnection = await Connect.findByIdAndDelete(req.params.id);
     if (!deletedConnection)
       return res.status(404).json({message: "Connection not found"});
     res.json({message: "Connection deleted successfully"});
-  }catch (err){
+  } catch (err){
     res.status(500).json({message: err.message});
   }
 };
--- functions/controllers/propertyController.js
+++ functions/controllers/propertyController.js
@@ -2,29 +2,29 @@ const Property = require("../models/Property");
 
 // Get all properties
 exports.getAllProperties = async (req, res) =>{
-  try{
+  try {
     const properties = await Property.find();
     res.json(properties);
-  }catch (err){
+  } catch (err){
     res.status(500).json({message: err.message});
   }
 };
 
 // Get property by ID
 exports.getPropertyById = async (req, res) =>{
-  try{
+  try {
     const property = await Property.findById(req.params.id);
     if (!property)
       return res.status(404).json({message: "Property not found"});
     res.json(property);
-  }catch (err){
+  } catch (err){
     res.status(500).json({message: err.message});
   }
 };
 
 // Create a new property
 exports.createProperty = async (req, res) =>{
-  try{
+  try {
     const newProperty = new Property(req.body);
     const savedProperty = await newProperty.save();
     res.status(201).json(savedProperty);
@@ -35,7 +35,7 @@ exports.createProperty = async (req, res) =>{
 
 // Update a property by ID
 exports.updateProperty = async (req, res) =>{
-  try{
+  try {
     const updatedProperty = await Property.findByIdAndUpdate(
       req.params.id,
       req.body,
@@ -44,19 +44,19 @@ exports.updateProperty = async (req, res) =>{
     if (!updatedProperty)
       return res.status(404).json({message: "Property not found"});
     res.json(updatedProperty);
-  }catch (err){
+  } catch (err){
     res.status(400).json({message: err.message});
   }
 };
 
 // Delete a property by ID
 exports.deleteProperty = async (req, res) =>{
-  try{
+  try {
     const deletedProperty = await Property.findByIdAndDelete(req.params.id);
     if (!deletedProperty)
       return res.status(404).json({message: "Property not found"});
     res.json({message: "Property deleted successfully"});
-  }catch (err){
+  } catch (err){
     res.status(500).json({message: err.message});
   }
 };
