42280fd4c7358f45b0fd3dcad98e73a7b824e8a9
Rajkumar Verma <rajkumarshaw624@gmail.com>
2024-12-14 20:12:36 +0530

    Remove Space

--- functions/routes/connectRoutes.js
+++ functions/routes/connectRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const connectController = require("../controllers/connectController");
 
-router.get("/", connectController.getAllConnections);        // GET all connections
-router.get("/:id", connectController.getConnectionById);     // GET by ID
-router.post("/", connectController.createConnection);        // POST (Create)
-router.put("/:id", connectController.updateConnection);      // PUT (Update)
-router.delete("/:id", connectController.deleteConnection);   // DELETE
+router.get("/", connectController.getAllConnections); // GET all connections
+router.get("/:id", connectController.getConnectionById); // GET by ID
+router.post("/", connectController.createConnection); // POST (Create)
+router.put("/:id", connectController.updateConnection); // PUT (Update)
+router.delete("/:id", connectController.deleteConnection); // DELETE
 
 module.exports = router;
